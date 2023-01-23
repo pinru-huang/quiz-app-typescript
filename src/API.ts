c7c6609e6c57e93e9de62d8fb071df9a2b6d55f4
Pinru Huang <b10901171@g.ntu.edu.tw>
2023-01-23 23:23:53 +0800

    start button selective showing

--- src/API.ts
+++ src/API.ts
@@ -4,7 +4,7 @@ export type Question = {
     category: string;
     correct_answer: string;
     difficulty: string;
-    incorrect_answer: string[];
+    incorrect_answers: string[];
     question: string;
     type: string;
 }
@@ -24,7 +24,7 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
     return data.results.map((question: Question) => (
         {
             ...question,
-            answer: shuffleArray([...question.incorrect_answer, question.correct_answer])
+            answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
         }
     ))
 }
