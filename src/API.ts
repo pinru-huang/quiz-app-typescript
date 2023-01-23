87049e568020c32134a78b6861b9f42189b409ff
Pinru Huang <b10901171@g.ntu.edu.tw>
2023-01-23 23:32:56 +0800

    Questions panel

--- src/API.ts
+++ src/API.ts
@@ -9,7 +9,7 @@ export type Question = {
     type: string;
 }
 
-export type QuestionState = Question & { answers: string; }
+export type QuestionState = Question & { answers: string[]; }
 
 export enum Difficulty {
     EASY = "easy",
